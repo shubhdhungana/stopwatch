3b3934c5f3433275ee73f19b80be88e12b2539df
Shubham Dhungana <shubhdhungana@gmail.com>
2024-05-15 17:54:42 +0545

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Footer from './Footer';
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(()=>{
+    let interval;
+    if(running){
+      interval = setInterval(()=>{
+        setTime((prevTime)=>prevTime + 10)
+      }, 10)
+    }else if(!running){
+      clearInterval(interval)
+    }
+    return ()=>clearInterval(interval)
+  }, [running])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Stopwatch</h1>
+      <div>
+        <span>{("0" + Math.floor((time / 60000))).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 10) % 100)).slice(-2)}</span>
+      </div>
+      <div>
+        {running ? (<button onClick={()=>{setRunning(false)}}>Stop</button>
+      ): ( 
+
+        <button onClick={()=>{setRunning(true)}}>Start</button>
+      )}
+        <button onClick={()=>{setTime(0)}}>Reset</button>
+      </div>
+      <Footer />  
+    </>
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>Developed By Subh</p>
+    </div>
+  );
+};
+
+export default Footer;
